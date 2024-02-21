d651e509da97ce37cabe8e8b2e848960a05c06b5
judeclark19 <judeclark2019@gmail.com>
2024-02-20 23:19:19 -0500

    Icons and manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,13 @@ const questrial = Questrial({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "V-Cell",
-  description: "Game concept by Jim Fox - Web app by Jude Clark"
+  description: "Game concept by Jim Fox - Web app by Jude Clark",
+  icons: {
+    icon: ["/favicon.ico"],
+    apple: ["/apple-touch-icon.png"],
+    shortcut: ["/apple-touch-icon.png"]
+  },
+  manifest: "/site.webmanifest"
 };
 
 export default function RootLayout({
