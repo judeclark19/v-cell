25e0a41ca3dd4da0b26f2f100416004e8bf94f88
judeclark19 <judeclark2019@gmail.com>
2025-06-09 14:32:14 -0400

    footer date change

--- app/Home.tsx
+++ app/Home.tsx
@@ -96,7 +96,7 @@ export default function Home() {
             </strong>
           </div>
         </div>
-        <div className="bottom">Last updated February 22, 2025</div>
+        <div className="bottom">Last updated June 9, 2025</div>
       </Footer>
     </PageWrapper>
   );
